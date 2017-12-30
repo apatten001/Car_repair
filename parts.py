86213d802c852342a1b32743277f3e1ceaa54620
Arnold <apatten001@yahoo.com>
2017-12-30 13:58:24 -0500

    Add files via upload

--- parts.py
+++ parts.py
@@ -1,6 +1,12 @@
 
 
-parts = {"Engine":4000, "Tire":169, "Alternator": 230, "Sparkplugs": 12, "Wires": 52 ,"Battery": 99}
+part = {"Engine": 4000, "Tire":169, "Alternator": 230, "Sparkplugs": 12, "Wires": 52 ,"Battery": 99}
+
+for key, val in part.items():
+    if key in part:
+        break
+    print(f"The {key} will costs ${val}")
+#
 
 
 
