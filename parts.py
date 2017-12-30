2f61aecc5e3ad0e544bcb441d92252d166e26640
Arnold <apatten001@yahoo.com>
2017-12-30 13:59:06 -0500

    Update parts.py

--- parts.py
+++ parts.py
@@ -2,11 +2,11 @@
 
 part = {"Engine": 4000, "Tire":169, "Alternator": 230, "Sparkplugs": 12, "Wires": 52 ,"Battery": 99}
 
-for key, val in part.items():
-    if key in part:
-        break
-    print(f"The {key} will costs ${val}")
-#
+# for key, val in part.items():
+#     if key in part:
+#         break
+#     print(f"The {key} will costs ${val}")
+
 
 
 
